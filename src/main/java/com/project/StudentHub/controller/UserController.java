844a3f2bd07c0d0937480299801f4ba4b0d8eb24
Codruta Dudau <codrutandudau@gmail.com>
2021-04-04 22:03:05 +0300

    changes

--- src/main/java/com/project/StudentHub/controller/UserController.java
+++ src/main/java/com/project/StudentHub/controller/UserController.java
@@ -2,7 +2,6 @@ package com.project.StudentHub.controller;
 
 import com.project.StudentHub.repository.UserRepository;
 import com.project.StudentHub.model.User;
-import com.project.StudentHub.model.UserUpdate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -40,32 +39,6 @@ public class UserController {
         if(!userOptional.isPresent())
             return ResponseEntity.notFound().build();
         user.setId(id);
-        userInterface.save(user);
-        return ResponseEntity.noContent().build();
-    }
-
-    @PatchMapping("/user/{id}")
-    public ResponseEntity<Object> partialUserUpdate(@RequestBody UserUpdate userUpdate, @PathVariable Integer id){
-        Optional<User> userOptional = userInterface.findById(id);
-        if(!userOptional.isPresent())
-            return ResponseEntity.notFound().build();
-
-        User user = userOptional.get();
-        if(userUpdate.getFirstName() != null){
-            user.setFirstName((userUpdate.getFirstName()));
-        }
-        if(userUpdate.getLastName() != null){
-            user.setLastName((userUpdate.getLastName()));
-        }
-        if(userUpdate.getEmail() != null){
-            user.setEmail((userUpdate.getEmail()));
-        }
-        if(userUpdate.getPassword() != null){
-            user.setPassword((userUpdate.getPassword()));
-        }
-        if(userUpdate.getPhoneNumber() != null){
-            user.setPhoneNumber((userUpdate.getPhoneNumber()));
-        }
         userInterface.save(user);
         return ResponseEntity.noContent().build();
     }

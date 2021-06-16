153efe9c7d3e2ff1a946dd5d8b55b355ce11e775
Codruta Dudau <codrutandudau@gmail.com>
2021-06-16 22:19:12 +0300

    add cotroller for roles + privileges

---
+++ src/main/java/com/project/StudentHub/controller/PrivilegeController.java
@@ -0,0 +1,49 @@
+package com.project.StudentHub.controller;
+
+import com.project.StudentHub.exception.ResourceNotFoundException;
+import com.project.StudentHub.model.Privilege;
+import com.project.StudentHub.model.Question;
+import com.project.StudentHub.repository.PrivilegeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api")
+@Validated
+public class PrivilegeController {
+    @Autowired
+    private PrivilegeRepository privilegeRepository;
+
+    @PostMapping("/privileges")
+    public Privilege addPrivilege(@Valid @RequestBody Privilege privilege){
+        return privilegeRepository.save(privilege);
+    }
+
+    @GetMapping("/privileges")
+    public Iterable<Privilege> privileges(){
+        return privilegeRepository.findAll();
+    }
+
+    @DeleteMapping("/privileges/{id}")
+    public void deleteQuestion(@PathVariable Integer id){
+        Privilege questionDelete = Optional.ofNullable(privilegeRepository.findPrivilegeById(id))
+                .orElseThrow(() -> new ResourceNotFoundException("Privilege with id: " + id + " not found"));
+        privilegeRepository.delete(questionDelete);
+    }
+
+    @PutMapping("/privileges/{id}")
+    public ResponseEntity<Object> updatePrivilege(@Valid @RequestBody Privilege privilege, @PathVariable Integer id){
+        Optional<Privilege> privilegeOptional = Optional.ofNullable(Optional.ofNullable(privilegeRepository.findPrivilegeById(id))
+                .orElseThrow(() -> new ResourceNotFoundException("Question with id: " + id + " not found")));
+        if(!privilegeOptional.isPresent())
+            return ResponseEntity.notFound().build();
+        privilege.setId(id);
+        privilegeRepository.save(privilege);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ src/main/java/com/project/StudentHub/controller/RoleController.java
@@ -0,0 +1,49 @@
+package com.project.StudentHub.controller;
+
+import com.project.StudentHub.exception.ResourceNotFoundException;
+import com.project.StudentHub.model.Question;
+import com.project.StudentHub.model.Role;
+import com.project.StudentHub.repository.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api")
+@Validated
+public class RoleController {
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @PostMapping("/roles")
+    public Role addRole (@Valid @RequestBody Role role){
+        return roleRepository.save(role);
+    }
+
+    @GetMapping("/roles")
+    public Iterable<Role> roles(){
+        return roleRepository.findAll();
+    }
+
+    @DeleteMapping("/roles/{id}")
+    public void deleteRole(@PathVariable Integer id){
+        Role roleDelete = Optional.ofNullable(roleRepository.findRoleById(id))
+                .orElseThrow(() -> new ResourceNotFoundException("Role with id: " + id + " not found"));
+        roleRepository.delete(roleDelete);
+    }
+
+    @PutMapping("/roles/{id}")
+    public ResponseEntity<Object> updateRole(@Valid @RequestBody Role role, @PathVariable Integer id){
+        Optional<Role> roleOptional = Optional.ofNullable(Optional.ofNullable(roleRepository.findRoleById(id))
+                .orElseThrow(() -> new ResourceNotFoundException("Role with id: " + id + " not found")));
+        if(!roleOptional.isPresent())
+            return ResponseEntity.notFound().build();
+        role.setId(id);
+        roleRepository.save(role);
+        return ResponseEntity.noContent().build();
+    }
+}

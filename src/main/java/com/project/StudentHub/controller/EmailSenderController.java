89c13e498b563c74625ebfcb4b4b6b11afc352a8
codrutadudau <codrutandudau@gmail.com>
2021-06-26 13:43:33 +0300

    fix email

--- src/main/java/com/project/StudentHub/controller/EmailSenderController.java
+++ src/main/java/com/project/StudentHub/controller/EmailSenderController.java
@@ -3,11 +3,15 @@ package com.project.StudentHub.controller;
 import com.project.StudentHub.configuration.email.EmailConfiguration;
 import com.project.StudentHub.dto.EmailSenderDto;
 import com.project.StudentHub.model.User;
+import com.project.StudentHub.repository.UserRepository;
+import com.project.StudentHub.services.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.xml.bind.ValidationException;
 
 @RestController
 @RequestMapping("/api")
@@ -15,15 +19,21 @@ import javax.validation.Valid;
 public class EmailSenderController {
     private EmailConfiguration emailConfiguration;
 
+    @Autowired
+    private EmailService emailService;
+
+    @Autowired
+    private UserRepository userRepository;
+
     @PostMapping("/emails/send")
-    public ResponseEntity<Object> sendEmail(@Valid @RequestBody EmailSenderDto emailSenderDto){
-        User user = new User();
-        emailSenderDto.setFrom(user.getEmail());
-        emailSenderDto.setTo(user.getEmail());
-        emailSenderDto.setSubject(user.getFirstName()+" " + user.getLastName() + " has registered");
-        emailSenderDto.setBody("Email text to be replaced");
+    public ResponseEntity<Object> sendEmail(@Valid @RequestBody EmailSenderDto emailSenderDto) throws ValidationException {
+        emailService.sendEmail(
+                userRepository.findByEmail(emailSenderDto.getFrom()),
+                userRepository.findByEmail(emailSenderDto.getTo()),
+                emailSenderDto.getSubject(),
+                emailSenderDto.getBody()
+        );
 
         return ResponseEntity.noContent().build();
     }
-
 }
